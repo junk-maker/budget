f3f7dadaab025002e01e24e229fe7037ce5f3fb1
Funny Doctor <funnydoctor89@gmail.com>
2021-06-23 12:39:10 +0300

    Did statistic page and some refactoring

--- frontend/src/components/statistic/Statistic.js
+++ frontend/src/components/statistic/Statistic.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Dropdown from '../presentation/ui/dropdown/Dropdown';
+import statisticStorage from '../../json-storage/statisticStorage.json';
 
 
-const Statistic = () => <div>Statistic</div>;
+const Statistic = () => {
+    return(
+        <div className={'statistic'}>
+            <div className={'statistic__header'}>
+                <div className={'statistic__header--title'}>Статистика</div>
+            </div>
+
+            <div className={'statistic__dropdown'}>
+                <Dropdown
+                    options={statisticStorage}
+                    placeholder={'Выбрать статистику'}
+                />
+            </div>
+        </div>
+    );
+};
 
 
 export default Statistic;
