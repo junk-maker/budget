2bbdca53161187be9f182ee453ec11ab00d1fef2
Funny Doctor <funnydoctor89@gmail.com>
2022-04-01 09:43:08 +0300

    Updated versions of packages and some refactoring

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = async yellow => {
-    await mongoose.connect(process.env.MONGO_URI, {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-    });
-    console.log(yellow,'MongoDB connection SUCCESS!');
-};
+const connectDB = async yellow => await mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log(yellow,'MongoDB connection SUCCESS!'));
 
 module.exports = connectDB;
