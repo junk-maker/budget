d4346c9491fb84749f89d2f12d519799f336f602
Funny Doctor <funnydoctor89@gmail.com>
2022-05-27 11:29:20 +0300

    fixed budget data response

--- backend/routers/routerForBudget.js
+++ backend/routers/routerForBudget.js
@@ -3,9 +3,9 @@ const {protectedRoute} = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 
-router.route('/budget').put(protectedRoute, editItem);
-router.route('/budget').post(protectedRoute, addItem);
-router.route('/budget/:id').delete(protectedRoute, deleteItem);
+router.route('/budget/:currency').put(protectedRoute, editItem);
+router.route('/budget/:currency').post(protectedRoute, addItem);
 router.route('/budget/:currency').get(protectedRoute, getBudget);
+router.route('/budget/:id/:currency').delete(protectedRoute, deleteItem);
 
 module.exports = router;
