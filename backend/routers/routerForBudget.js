0db796513013a35dd0b52cddac021625062ae379
Funny Doctor <funnydoctor89@gmail.com>
2022-06-22 17:39:50 +0300

    Refactoring datepicker API

--- backend/routers/routerForBudget.js
+++ backend/routers/routerForBudget.js
@@ -3,9 +3,9 @@ const {protectedRoute} = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
 
-router.route('/budget/:currency').put(protectedRoute, editItem);
-router.route('/budget/:currency').post(protectedRoute, addItem);
-router.route('/budget/:id/:currency').delete(protectedRoute, deleteItem);
+router.route('/budget/:end/:start/:year/:month/:currency').put(protectedRoute, editItem);
+router.route('/budget/:end/:start/:year/:month/:currency').post(protectedRoute, addItem);
 router.route('/budget/:end/:start/:year/:month/:currency').get(protectedRoute, getBudget);
+router.route('/budget/:id/:end/:start/:year/:month/:currency').delete(protectedRoute, deleteItem);
 
 module.exports = router;
